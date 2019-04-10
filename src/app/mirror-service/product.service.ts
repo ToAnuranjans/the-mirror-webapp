b3030979b7e649c132287aaef25a12e948d900a3
byanuranjans <toanuranjans@hotmail.com>
2019-04-10 18:16:56 +0530

    Url changed

--- src/app/mirror-service/product.service.ts
+++ src/app/mirror-service/product.service.ts
@@ -4,11 +4,13 @@ import { HttpClient, HttpEventType } from "@angular/common/http";
 import { IProduct } from "../model/product-model";
 import { Observable } from "rxjs";
 
+import {environment} from "../../environments/environment";
+
 @Injectable({
   providedIn: "root"
 })
 export class ProductService {
-  url = "http://localhost:3000/products";
+  url = `${environment.theMirrorApi}/products`;
 
   constructor(private http: HttpClient) {}
 
